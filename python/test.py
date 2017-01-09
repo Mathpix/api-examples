a661c01bf5d6abf81da927db8d71e93a263c96b3
Nicolas D. Jimenez <nicodjimenez@gmail.com>
2017-01-09 09:45:32 +0800

    Indent returned JSON.

--- python/test.py
+++ python/test.py
@@ -14,4 +14,4 @@ r = requests.post('http://api.mathpix.com/v2/latex',
     files={'file': open(file_path, 'rb')}, 
     headers={"app_id": "4985f625", "app_key": "4423301b832793e217d04bc44eb041d3"})
 text = r.text
-print text
+print json.dumps(json.loads(text), indent=4, sort_keys=True)
