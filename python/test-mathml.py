b38653e73be81dbb4247df7324e84e9f61f191ba
Mark Linton <mark@mathpix.com>
2018-02-14 15:45:12 -0800

    Use updated API.

--- python/test-mathml.py
+++ python/test-mathml.py
@@ -7,7 +7,7 @@ import json
 file_path = sys.argv[1]
 image_uri = "data:image/jpg;base64," + base64.b64encode(open(file_path, "rb").read())
 r = requests.post("https://api.mathpix.com/v3/latex",
-    data=json.dumps({'url': image_uri, 'formats': {'mathml': True}}),
+    data=json.dumps({'src': image_uri, 'formats': ['mathml']}),
     headers={"app_id": "trial", "app_key": "34f1a4cea0eaca8540c95908b4dc84ab",
         "Content-type": "application/json"})
 print json.loads(r.text)['mathml']
