844e9ac281f1f827fecb18ebaf57b5b62ef576d2
Mark Linton <mark@mathpix.com>
2019-08-02 07:59:18 -0700

    Add mathml to formats, remove separate test-mathml example. [skip ci]

--- python/formats.py
+++ python/formats.py
@@ -6,26 +6,35 @@ import json
 #
 # Example using Mathpix OCR with multiple result formats. We want to recognize
 # both math and text in the image, so we pass the ocr parameter set to
-# ['math', 'text']. This example returns both the text format, which
-# starts in text mode instead of math mode, and the latex_styled format.
-# We also define custom math delimiters for the text result so that
-# the math is surrounded by dollar signs ("$").
+# ['math', 'text']. This example returns the LaTeX text format, which
+# starts in text mode instead of math mode, the latex_styled format,
+# the asciimath format, and the mathml format. We define custom
+# math delimiters for the text result so that the math is surrounded
+# by dollar signs ("$").
 #
 
 r = mathpix.latex({
     'src': mathpix.image_uri('../images/mixed_text_math.jpg'),
     'ocr': ['math', 'text'],
     'skip_recrop': True,
-    'formats': ['text', 'latex_styled'],
+    'formats': ['text', 'latex_styled', 'asciimath', 'mathml'],
     'format_options': {
         'text': {
             'transforms': ['rm_spaces', 'rm_newlines'],
             'math_delims': ['$', '$']
         },
-        'latex_styled': {'transforms': ['rm_spaces']},
+        'latex_styled': {'transforms': ['rm_spaces']}
     }
 })
 
-print(json.dumps(r, indent=4, sort_keys=True))
-assert(r['text'] == '$-10 x^{2}+5 x-3$ and $-7 x+4$')
-assert(r['latex_styled'] == '-10 x^{2}+5 x-3 \\text { and }-7 x+4')
+#
+# Note the actual results might be slighly different in LaTeX spacing or
+# MathML attributes.
+#
+
+print('Expected for r["text"]: "$-10 x^{2}+5 x-3$ and $-7 x+4$"')
+print('Expected for r["latex_styled"]: "-10 x^{2}+5 x-3 \\text { and }-7 x+4"')
+print('Expected for r["asciimath"]: "-10x^(2)+5x-3\\" and \\"-7x+4"')
+print('Expected for r["mathml"]: "<math><mo>\u2212</mo><mn>10</mn><msup><mi>x</mi><mn>2</mn></msup><mo>+</mo><mn>5</mn><mi>x</mi><mo>\u2212</mo><mn>3</mn><mtext>\u00a0and\u00a0</mtext><mo>\u2212</mo><mn>7</mn><mi>x</mi><mo>+</mo><mn>4</mn></math>"')
+
+print("\nResult object:", json.dumps(r, indent=4, sort_keys=True))
--- python/simple.py
+++ python/simple.py
@@ -8,6 +8,13 @@ import json
 #
 # We use the default ocr (math-only) and a single return format, latex_simplified.
 #
+# If you expect the image to be math and want to examine the result
+# as math then the return format should either be latex_simplified,
+# asciimath, or mathml. If you want to see the text in the image then
+# you should include 'ocr': ['math', 'text'] as an argument and
+# the return format should be either text or latex_styled
+# (depending on whether you want to use the result as a paragraph or an equation).
+#
 
 r = mathpix.latex({
     'src': mathpix.image_uri('../images/algebra.jpg'),
